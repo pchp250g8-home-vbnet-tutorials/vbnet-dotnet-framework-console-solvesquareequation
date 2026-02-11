4f8b5925e829859403926d98b9a385713fa23b56
Alexander <pchp250g8@tutamail.com>
2026-02-11 15:55:52 +0300

    Добавьте файлы проекта.

---
+++ SolveSquareEquation/Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+		Console.Clear()
+		Console.WriteLine("Input coefficients of lineal equation")
+		Dim a As Single
+		Single.TryParse(Console.ReadLine(), a)
+		Dim b As Single
+		Single.TryParse(Console.ReadLine(), b)
+		Dim c As Single
+		Single.TryParse(Console.ReadLine(), c)
+		Console.WriteLine("Trying to solve the equation:{0} * x ^ 2 + {1} * x + {2} = 0", a, b, c)
+		If a = 0 Then
+			If (b = 0) AndAlso (c = 0) Then
+				Console.WriteLine("The answer is any number")
+			ElseIf (b = 0) AndAlso (c <> 0) Then
+				Console.WriteLine("The equation has no roots")
+			Else
+				Console.WriteLine("The equation has the following root:{0}", -c / b)
+			End If
+		Else
+			Dim d = b * b - 4 * a * c
+			Dim nRoots = Math.Sign(d) + 1
+			If nRoots > 0 Then
+				Dim x1 = (-b - Math.Sqrt(d)) / (2 * a)
+				Dim x2 = (-b + Math.Sqrt(d)) / (2 * a)
+				Console.WriteLine("The equation has the following roots:")
+				Console.WriteLine("x1 = {0}", x1)
+				Console.WriteLine("x2 = {0}", x2)
+			Else
+				Console.WriteLine("The equation has no roots")
+			End If
+		End If
+		Console.Read()
+	End Sub
+
+End Module
---
+++ SolveSquareEquation/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("SolveSquareEquation")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SolveSquareEquation")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("d0c9343c-107e-45b3-a2f6-7dd4ab1a4421")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
